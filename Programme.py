dea56e5fd4b6f943942303d11c0cc6393903f1e3
uvsq22200395 <114587394+uvsq22200395@users.noreply.github.com>
2023-04-02 14:51:51 +0200

    Update Programme.py

--- Programme.py
+++ Programme.py
@@ -1,17 +1,23 @@
 #code_césarTessa
-alphabetMaj = ["A", "B", "C", "D", "E","F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Z"]
-alphabetmin = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y","z"]
-def cryptage2(cle, lettre) : 
-    #Si la lettre est en majuscule on utilise la méthode ord() ce qui donne la table des lettres
-    if 65 <= ord(lettre) <= 90 :
-        return chr(65 + (ord(lettre) - 65 + cle)%26) #%26 permet de se balader dans l'alphabet, -65 permet de donner la position de la lettre
+def cryptage(message, cle) : 
+    message_chiffre = ""
+    #Si la lettre est en majuscule on utilise la méthode ord() ce qui donne la table des lettres ASCII
+    for lettre in message:
+        if 65 <= ord(lettre) <= 90 :
+            lettre_chiffree = chr(65 + (ord(lettre) - 65 + cle)%26) #%26 permet de se balader dans l'alphabet, -65 permet de donner la position de la lettre
                                                      #cle permet le décalage de la lettre, +65 permet de trouver le décalage de la lettre dans la table
-    
     #Si la lettre est en minuscule
-    elif 97 <= ord(lettre) <= 122 :
-        return chr(97 + (ord(lettre) - 97 + cle)% 26)
-    else :
-        return lettre
+        elif 97 <= ord(lettre) <= 122 :
+            lettre_chiffree = chr(97 + (ord(lettre) - 97 + cle)% 26)
+        else :
+            lettre_chiffree = lettre
+        message_chiffre += lettre_chiffree
+    return message_chiffre
+
+message = "Salut, moi c'est Tessa"
+decalage = 3
+message_chiffre = cryptage(message, decalage)
+print("Message chiffré:", message_chiffre)
     
 
 
